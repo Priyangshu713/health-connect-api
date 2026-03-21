9491c3ce6743281a803e4bff7c15cba6df8a0a9c
Priyangshu713 <priyangshudutta2003@gmail.com>
2026-03-21 19:09:50 +0530

    fix: allow x-user-tier header in CORS and update allowed origins

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ const corsOptions = {
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-user-tier']
 }
 
 // CORS must be applied before any other middleware
